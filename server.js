0e6737d80936529b5b91adfb395521580e774022
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-06 19:20:12 -0700

    setting up potential necessary constants and express package

---
+++ server.js
@@ -0,0 +1,12 @@
+// Const;ants
+const inquirer = require("inquirer");
+const fs = require("fs");
+const express = require("express");
+
+// Pulled from Activity 10
+// Consts for using Path 
+// Initializing express
+const path = require("path");
+const app = express();
+// Need a Port to host site on
+const PORT = process.env.port || 3001;
